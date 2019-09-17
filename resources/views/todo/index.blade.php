e0edf8e3785cf6ebce983acafa9d167084d42cc5
S-Naoki <s.nao0919@gmail.com>
2019-09-17 10:09:30 +0900

    インデントの修正をしました

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -19,8 +19,7 @@
   <tbody>
     @foreach ($todos as $todo)
       <tr>
-        <td class="align-middle">{{ 
-        $todo->id }}</td>
+        <td class="align-middle">{{ $todo->id }}</td>
         <td class="align-middle">{{ $todo->title }}</td>
         <td class="align-middle">{{ $todo->created_at }}</td>
         <td class="align-middle">{{ $todo->updated_at }}</td>
